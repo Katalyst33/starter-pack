e1a625acc60f6376f9bfc203d873ded3d23c8b1b
hibrid <samuelnmeje@gmail.com>
2019-07-30 10:31:49 +0100

    added global and local component

---
+++ src/components/commons/FooterArea.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="footer">
+        <div class="container ">
+            <div class="row">
+                <div class="col-4">
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                </div>
+                <div class="col-4">
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                </div>
+                <div class="col-4">
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                    <p>ONe item</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
